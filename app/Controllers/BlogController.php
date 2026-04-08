5a1e27127e66cd94376d73d1f8b0117feb85257e
Kazimir <kazimirdev@tuta.io>
2026-04-08 12:22:41 +0200

    get locale and load db model

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -1,6 +1,12 @@
 <?php
     class BlogController {
         public function index() {
+
+            $GLOBALS['locale'] = $_GET['locale'] ?? 'en';
+
+            $postModel = new Posts();
+            $posts = $postModel->getAllByLocale($GLOBALS['locale']);
+
             require __DIR__ . '/../Views/blog/index.php';
         }
     }
