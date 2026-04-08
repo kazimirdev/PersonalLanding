4b9b919cacb3e86cf09fa399fb97f1c5236b5bee
Kazimir <kazimirdev@tuta.io>
2026-04-08 12:24:16 +0200

    try db connect until limit

--- app/Core/DB.php
+++ app/Core/DB.php
@@ -6,17 +6,33 @@ class DB {
 
     public static function connect(): PDO {
         if (self::$pdo === null) {
-            require __DIR__ . '../../config/db.php'; // Load database configuration
+            $config = require __DIR__ . '/../../config/db.php'; // Load database configuration
 
-            self::$pdo = new PDO(
-                $config['dsn'], 
-                $config['user'], 
-                $config['pass'],
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions on errors
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
-                    PDO::ATTR_EMULATE_PREPARES => false, // Use native prepared statements if supported
-                ]);
+            $attempts = 0;
+
+            while ($attempts < 10) {
+                try {
+                    self::$pdo = new PDO(
+                        $config['dsn'], 
+                        $config['user'], 
+                        $config['pass'],
+                        [
+                            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Throw exceptions on errors
+                            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
+                            PDO::ATTR_EMULATE_PREPARES => false, // Use native prepared statements if supported
+                        ]);
+                    break; // Connection successful, exit loop
+                } catch (PDOException $e) {
+                    error_log("Database connection failed: " . $e->getMessage());
+                    $attempts++;
+                    sleep(1*$attempts); // Wait before retrying
+
+                    if ($attempts === 10) {
+                        throw new Exception("Unable to connect to the database after multiple attempts.\n Error: " . $e->getMessage());
+                    }
+                }
+            }
+
         }
         return self::$pdo;
     }
