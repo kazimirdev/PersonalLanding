a0b36bf69bc445f1711d16966d5e82e535805acb
Kazimir <kazimirdev@tuta.io>
2026-05-10 00:36:21 +0200

    Add admin helpers, lang, and middlewares

---
+++ admin-app/Lang/en.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'website_links' => 'website Links',
+    'social_links' => 'social Links',
+    'content' => 'content',
+    'products' => 'products',
+];
+?>
---
+++ admin-app/Lang/pl.php
@@ -0,0 +1,9 @@
+<?php 
+return [
+    'website_links' => 'linki wewnętrzne',
+    'social_links' => 'linki zewnętrzne',
+    'content' => 'treść',
+    'products' => 'produkty',
+];
+
+?>
