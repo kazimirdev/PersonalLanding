305029121af98ae5811449327b1e51eab46d95cd
Kazimir <kazimirdev@tuta.io>
2026-05-12 22:21:28 +0200

    adaptive grid | content

--- website/app/Views/layouts/blogfeed.php
+++ website/app/Views/layouts/blogfeed.php
@@ -1,18 +1,19 @@
 <main>
     <div class="content-feed">
+        <?php foreach ($posts as $post): ?>
+        <a href="/content/<?= $post['slug'] ?>" class="blog-post">
         <div class="content-post">
-            <?php foreach ($posts as $post): ?>
-                <a href="/content/<?= $post['slug'] ?>" class="blog-post">
-                    <h3><?= $post['title'] ?></h3>
-                    <?php if ($post['image_preview_url']): ?>
-                        <img src="<?= $post['image_preview_url'] ?>" alt="Preview Image" class="blog-post-preview">
-                    <?php endif; ?>
-                    <?php if (strlen($post['content_html'] ?? '') > 200): ?>
-                        <p><?= substr($post['content_html'], 0, 200) ?>...</p>
-                    <?php else: ?>
-                        <p><?= $post['content_html'] ?></p>
-                    <?php endif; ?>
-                </a>
-            <?php endforeach; ?>
+                <h3><?= $post['title'] ?></h3>
+                <?php if ($post['image_preview_url']): ?>
+                    <img src="<?= $post['image_preview_url'] ?>" alt="Preview Image" class="blog-post-preview">
+                <?php endif; ?>
+                <?php if (strlen($post['content_html'] ?? '') > 200): ?>
+                    <p><?= substr($post['content_html'], 0, 200) ?>...</p>
+                <?php else: ?>
+                    <p><?= $post['content_html'] ?></p>
+                <?php endif; ?>
         </div>
+        </a>
+        <?php endforeach; ?>
+    </div>
 </main>
--- website/app/Views/layouts/head.php
+++ website/app/Views/layouts/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $page_title_head ?? "kazimir.dev"; ?></title>
+    <title><?php echo $page_title_head ?? "[kazimir.dev]"; ?></title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" href="/img/favicon.ico">
     <?php echo $additional_head ?? ''; ?>
