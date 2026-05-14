e49ff8de555026727ea40c7f882cb38b5a743a89
Kazimir <kazimirdev@tuta.io>
2026-05-14 22:59:28 +0200

    .

--- website/docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Substitute environment variables and copy to init directory
-sed "s/\\\${DB_NAME}/$DB_NAME/g" /config/create_schema.sql > /docker-entrypoint-initdb.d/01-create_schema.sql
-
-# Run the original MySQL entrypoint
-exec /usr/local/bin/docker-entrypoint.sh "$@"
