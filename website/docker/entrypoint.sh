9f0e755a4e87ac3a6711e2a9642afd5424cb3fa0
Kazimir <kazimirdev@tuta.io>
2026-05-14 22:42:47 +0200

    envsubst -> sed

--- website/docker/entrypoint.sh
+++ website/docker/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Substitute environment variables in SQL file
-envsubst < /docker-entrypoint-initdb.d/create_schema.sql > /docker-entrypoint-initdb.d/init.sql
+# Substitute environment variables in SQL file using sed
+sed "s/\${DB_NAME}/$DB_NAME/g" /docker-entrypoint-initdb.d/create_schema.sql > /docker-entrypoint-initdb.d/init.sql
 
 # Continue with MySQL startup
 exec "$@"
