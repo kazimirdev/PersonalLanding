46f35feeae1dba113d7e92b6fbebc034960a8a5a
Kazimir <kazimirdev@tuta.io>
2026-05-14 22:49:40 +0200

    .

--- website/docker/entrypoint.sh
+++ website/docker/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Check if source SQL file exists and process it
-if [ -f "/tmp/create_schema.sql" ]; then
-    sed "s/\${DB_NAME}/$DB_NAME/g" /tmp/create_schema.sql > /docker-entrypoint-initdb.d/create_schema.sql
-fi
+# Substitute environment variables and copy to init directory
+sed "s/\\\${DB_NAME}/$DB_NAME/g" /tmp/create_schema.sql > /docker-entrypoint-initdb.d/01-create_schema.sql
 
-# Continue with MySQL startup
-exec "$@"
+# Run the original MySQL entrypoint
+exec /usr/local/bin/docker-entrypoint.sh "$@"
