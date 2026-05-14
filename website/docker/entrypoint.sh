488904c8fda96965442861808787b3508ae42aba
Kazimir <kazimirdev@tuta.io>
2026-05-14 22:52:03 +0200

    .

--- website/docker/entrypoint.sh
+++ website/docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables and copy to init directory
-sed "s/\\\${DB_NAME}/$DB_NAME/g" /tmp/create_schema.sql > /docker-entrypoint-initdb.d/01-create_schema.sql
+sed "s/\\\${DB_NAME}/$DB_NAME/g" /config/create_schema.sql > /docker-entrypoint-initdb.d/01-create_schema.sql
 
 # Run the original MySQL entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
