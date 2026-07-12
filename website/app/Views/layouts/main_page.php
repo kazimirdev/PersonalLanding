bdeb5a541389b5077b72e8338611e98040df8de2
Kazimir <kazimirdev@tuta.io>
2026-07-12 18:34:12 +0200

    rm simplex link

--- website/app/Views/layouts/main_page.php
+++ website/app/Views/layouts/main_page.php
@@ -9,7 +9,6 @@
             <a href="https://github.com/kazimirdev" target="_blank"><p>github</p></a><br>
             <a href="https://t.me/kazimirdev" target="_blank"><p>telegram</p></a><br>
             <a href="https://linkedin.com/in/kazimirdev" target="_blank"><p>linkedin</p></a><br>
-            <a href="https://smp12.simplex.im/a#rLp0AjeoUzOcZ87mNXB3dBBGjtIyhQHhu5gmluP5MVA" target="_blank"><p>simplex</p></a><br>
             <a href="mailto:kazimirdev@tuta.io"><p>mail</p></a>
         </div>
 </main>
